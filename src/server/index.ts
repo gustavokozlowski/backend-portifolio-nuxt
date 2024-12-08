49ae979e5d04c9f8424a821b23fcafc16fd42c55
gustavokozlowski <guskozlowskidev@gmail.com>
2024-12-08 14:15:56 -0300

    :sparkles: feat(server): add cors middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
-import config from '../config/config.js';
+import config from '../config/enviroments/config.js';
+import admin from '../config/firebase/firebase.config.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(config.port, () => {
-  console.log(`Server rondando liso na porta ${config.port}`);
+  console.log(`Server rondando liso na porta ${config.port}\n Admin: ${admin}`);
 });
