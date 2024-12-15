23e22f65a9acd95f7bcf6d4213edd6aa6574b84b
gustavokozlowski <guskozlowskidev@gmail.com>
2024-12-15 04:12:35 -0300

    feat: add userRoutes

---
+++ src/api/routes/user/userRoutes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import * as userController from '../../controllers/user/userController.js';
+
+export const userRouter = Router();
+
+userRouter.route('/api/v1/user').get(userController.getAll);
+userRouter.route('/api/v1/user/:id').get(userController.getUser);
