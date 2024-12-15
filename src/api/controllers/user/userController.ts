73d98fd06798f8e058219bc3f91a09e9a177e34c
gustavokozlowski <guskozlowskidev@gmail.com>
2024-12-15 06:33:46 -0300

    feat: create async method addUser

--- src/api/controllers/user/userController.ts
+++ src/api/controllers/user/userController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from 'express';
-import { db } from '../../services/firebase/firebase.config.js';
+import type { AuthCreateUserRequest } from '../../services/firebase/types.js';
 import type { User } from './types.js';
+import { db } from '../../services/firebase/firebase.config.js';
+import { Admin } from '../../services/firebase/auth.services.js';
 
 const snap = await db.collection('users').get();
 
@@ -27,4 +29,42 @@ export const getUser = async (req: Request, res: Response) => {
     status: 'success',
     data: user,
   });
+};
+
+export const addUser = async (req: Request, res: Response) => {
+  try {
+    const admin = new Admin();
+    const params: AuthCreateUserRequest = {
+      displayName: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      phoneNumber: req.body.phoneNumber,
+      photoURL: req.body.photoURL,
+    };
+
+    const user = await admin.createUser(params);
+
+    const newUser: User = {
+      id: user.uid,
+      email: user.email as string,
+      name: user.displayName as string,
+      label: req.body.label,
+    };
+
+    const response = {
+      status: 'success',
+      data: user,
+    };
+
+    const data = await db.collection('users').doc(user.uid).set(newUser);
+
+    if (!data)
+      res.status(400).json({
+        status: 'Erro ao criar User, verifique os dados e tente novamente!',
+      });
+
+    res.status(201).json(response);
+  } catch (error) {
+    console.info('Deu merda pra criar o user:\n', error);
+  }
 };
