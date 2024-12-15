d922b9901ebd7d097e24dd29cdae85af7856e23e
gustavokozlowski <guskozlowskidev@gmail.com>
2024-12-15 04:13:12 -0300

    feat: use userRouter

--- src/api/server/index.ts
+++ src/api/server/index.ts
@@ -1,15 +1,14 @@
 import cors from 'cors';
 import express from 'express';
 import 'dotenv/config';
-import * as userController from '../controllers/user/userController.js';
+import { userRouter } from '../routes/user/userRoutes.js';
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.get('/api/v1/user', userController.getAll);
+app.use(userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server rondando liso na porta ${PORT}\n`);
