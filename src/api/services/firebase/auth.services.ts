ba843497cc0136f4b501a1b05799e481b1c33865
gustavokozlowski <guskozlowskidev@gmail.com>
2024-12-18 02:30:07 -0300

    fix: adjust interface name

--- src/api/services/firebase/auth.services.ts
+++ src/api/services/firebase/auth.services.ts
@@ -1,14 +1,14 @@
 import { getAuth, type UserRecord } from 'firebase-admin/auth';
-import type { AuthCreateUserRequest } from './types.js';
+import type { IAuthCreateUserParams } from './types.js';
 
 export class Admin {
-  async createUser(user: AuthCreateUserRequest): Promise<UserRecord> {
+  async createUser(user: IAuthCreateUserParams): Promise<UserRecord> {
     const auth = getAuth();
     const newUser = await auth.createUser({
       emailVerified: false,
       disabled: false,
       ...user,
     });
-   return newUser;
+    return newUser;
   }
 }
